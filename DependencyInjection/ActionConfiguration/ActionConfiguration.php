3876ec578ebf1ca819b201a4b7a1e02a9c71681c
Emeric Kasbarian <emeric.kasbarian@elao.com>
2014-10-15 15:09:28 +0200

    Moved action configurations to the DependencyInjection folder

--- DependencyInjection/ActionConfiguration/ActionConfiguration.php
+++ DependencyInjection/ActionConfiguration/ActionConfiguration.php
@@ -1,13 +1,22 @@
 <?php
 
-namespace Elao\Bundle\AdminBundle\Action\Configuration;
+/*
+ * This file is part of the AdeleBundleAdmin.
+ *
+ * (c) 2014 Elao <contact@elao.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Elao\Bundle\AdminBundle\DependencyInjection\ActionConfiguration;
 
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 
 /**
-* Action Configuration
-*/
+ * Action Configuration
+ */
 abstract class ActionConfiguration implements ConfigurationInterface
 {
     /**
--- DependencyInjection/ActionConfiguration/IndexActionConfiguration.php
+++ DependencyInjection/ActionConfiguration/IndexActionConfiguration.php
@@ -1,11 +1,20 @@
 <?php
 
-namespace Elao\Bundle\AdminBundle\Action\Configuration;
+/*
+ * This file is part of the "project".
+ *
+ * (c) 2014 Elao <contact@elao.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Elao\Bundle\AdminBundle\DependencyInjection\ActionConfiguration;
 
 /**
-* Index Action Configuration
-*/
- class IndexActionConfiguration extends ActionConfiguration
+ * Index Action Configuration
+ */
+class IndexActionConfiguration extends ActionConfiguration
 {
     /**
      * Get config tree builder
